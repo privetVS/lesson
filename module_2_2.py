4838d74a03184df2bcc6b5629d80ea2298067f06
privetVS <116426253+privetVS@users.noreply.github.com>
2024-08-14 17:24:40 +0700

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+first = int(input('Введите число 1: '))
+second = int(input('Введите число 2: '))
+third = int(input('Введите число 3: '))
+
+if first == second == third:
+    print(3)
+elif first == second or second == third or first == third:
+        print(1)
+
+else: print('0')
